21c4ef4ad339cefee9869a44ffdd176ba8ea12ea
Bruno Duarte <duartebruno2208@gmail.com>
2022-02-23 22:35:51 -0300

    add init function and resetting button

--- script.js
+++ script.js
@@ -14,14 +14,23 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -62,6 +71,7 @@ btnHold.addEventListener('click', () => {
     // 2. Check if player's score is >= 100
     if (scores[activePlayer] >= 100) {
       // Finish the game
+      diceEl.classList.add('hidden');
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -75,3 +85,5 @@ btnHold.addEventListener('click', () => {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
